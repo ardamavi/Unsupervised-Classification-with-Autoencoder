cb190b74d85ea525f4857c4c44329b7f60e51d7e
Arda Mavi <ardamavi2@gmail.com>
2017-12-08 20:42:54 +0300

    Added Dog-Cat classification example

---
+++ Examples/Dog-Cat/get_dataset.py
@@ -0,0 +1,40 @@
+# Arda Mavi
+import os
+import numpy as np
+from os import listdir
+from scipy.misc import imread, imresize
+from keras.utils import to_categorical
+from sklearn.model_selection import train_test_split
+
+def get_img(data_path):
+    # Getting image array from path:
+    img_size = 64
+    img = imread(data_path)
+    img = imresize(img, (img_size, img_size, 3))
+    return img
+
+def get_dataset(dataset_path='Data/Train_Data'):
+    # Getting all data from data path:
+    try:
+        X = np.load('Data/npy_train_data/X.npy')
+        Y = np.load('Data/npy_train_data/Y.npy')
+    except:
+        labels = listdir(dataset_path) # Geting labels
+        X = []
+        Y = []
+        for label in labels:
+            datas_path = dataset_path+'/'+label
+            for data in listdir(datas_path):
+                img = get_img(datas_path+'/'+data)
+                X.append(img)
+                Y.append(int(label))
+        # Create dateset:
+        X = np.array(X).astype('float32')/255.
+        Y = np.array(Y).astype('float32')
+        Y = to_categorical(Y, 2)
+        if not os.path.exists('Data/npy_train_data/'):
+            os.makedirs('Data/npy_train_data/')
+        np.save('Data/npy_train_data/X.npy', X)
+        np.save('Data/npy_train_data/Y.npy', Y)
+    X, X_test, Y, Y_test = train_test_split(X, Y, test_size=0.1, random_state=42)
+    return X, X_test, Y, Y_test
